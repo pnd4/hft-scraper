4be851465f552a7248061b273cb55b7fc0c5e4e7
Kevin Tran <cfcomfreak@yahoo.com>
2017-06-11 07:40:25 -0700

    Overhaul: Pretty much a rewrite of the entire project. Managed to fix DNS error with workaround from stackexchange (see comment).

--- hft-scrape.py
+++ hft-scrape.py
@@ -1,61 +1,126 @@
-## HFT Coupon Scraper (hft-scrape.py)
-# by Kevin 'pnd4' Tran
+#! python3
+# hft-scrape.py
 
-#!/usr/bin/python
-
-import requests
-import urllib
-import re
+import requests, re, urllib, csv
 from bs4 import BeautifulSoup
 
-url = "https://www.harborfreight.com/savings_coupons.html"
+def sitecheck(url):
+    status = None
+    message = ''
+    try:
+        resp = requests.head(url)
+        status = str(resp.status_code)
+    except requests.exceptions.NewConnectionError as exc:
+        # filtering DNS lookup error from other connection errors
+        # (until https://github.com/shazow/urllib3/issues/1003 is resolved)
+        if type(exc.message) != requests.packages.urllib3.exceptions.MaxRetryError:
+            raise
+        reason = exc.message.reason    
+        if type(reason) != requests.packages.urllib3.exceptions.NewConnectionError:
+            raise
+        if type(reason.message) != str:
+            raise
+        if ("[Errno 11001] getaddrinfo failed" in reason.message or     # Windows
+            "[Errno -2] Name or service not known" in reason.message or # Linux
+            "[Errno 8] nodename nor servname " in reason.message):      # OS X
+            message = 'DNSLookupError'
+        else:
+            raise
+    return url, status, message
 
-r = requests.get(url)
+# URL partitions
+u = '/savings_coupons.html'
+prefix = 'http://harborfreight.com'
 
-if r.status_code != 200:
-    print("ERROR: Request Denied")
+# Download the page.
+res = requests.get(prefix + u)
+try:
+    res.raise_for_status()
+except Exception as exc:
+    print('There was a problem: %s' % (exc))
+
+# Create soup
+s = BeautifulSoup(res.text, 'lxml')
+
+# Find coupons
+cpns = s.select('div[class^="coupon-container"]')
+if cpns == []:
+    print("Could not find a coupon element.")
 else:
-    print("item" + "|" + "desc" + "|" + "code")
-
-soup = BeautifulSoup(r.text, "lxml")
-
-## Find all <div> elements belonging to a 'coupon-container*' class, each is a coupon
-coupons = soup.find_all("div", re.compile("coupon-container*"))
-
-## Create dict to store data-groups
-## - catalog[0] = "12345"                                   -> dict-key, the items sku
-## - catalog[0]["skus"] = ['12345', '54321']                -> list of aggregate skus
-## - catalog[0]["desc"] = "4in Angle Grinder"               -> the sku's desc
-## - catalog[0]["barcode"] = "https://hft.com/12345.jpg"    -> coupon-barcode url
-## - catalog[0]["code"] = "987654321"                       -> coupon-code
-catalog = {}
-
-## Go through all coupons using each sku as a 'key'
-## 
-for coupon in coupons:
-    sku = coupon.find("a", attrs={"target": "_blank"})["href"].split("-")[-1].split(".")[-2]
-    catalog[sku] = {}
-    catalog[sku]["skus"] = []
-    catalog[sku]["desc"] = coupon.img["alt"]
-    catalog[sku]["barcode"] = coupon.find("img", attrs={"width": "188"})["src"]
-    catalog[sku]["code"] = catalog[sku]["barcode"].split("/")[-1].split(".")[-2]
-    q_keyword = ('"' + catalog[sku]["desc"] + '"')
-    q_url = "https://www.harborfreight.com/catalogsearch/result/index/?q="
-    aggsku_req = requests.get(q_url + q_keyword)
-    aggsku_soup = BeautifulSoup(aggsku_req.text, "lxml")
-    aggskus = aggsku_soup.find_all("div", class_="product-ids")
-    for aggsku in aggskus:
-        catalog[sku]["skus"].append(aggsku.get_text().split("#")[1])
-    for item_num in catalog[sku]["skus"]:
-        print('"' + item_num + '"|"' + catalog[sku]["desc"] + '"|"' + catalog[sku]["code"] + '"')
+    print("Found ", len(cpns), " coupons.\n")
     
-###
-#print("[Dict Node]\n",catalog['62368'], "\n\n")
+    # Open csv-file for write
+    csvFile = open('hft-spreadsheet.csv', 'w')
+    csvWriter = csv.writer(csvFile, dialect='excel', quotechar='"', delimiter='|', quoting=csv.QUOTE_ALL)
+    for c in cpns:
+        try:
+            # Select item-description
+            cText = c.img['alt']
+            print('Desc=', cText)
 
-## Print data from the Catalog
-#for product in catalog:
-#    print('"' + product + '"|"', end='')
-#    for item_num in catalog[product]["skus"]:
-#        print(item_num + ",", end='')
-#    print('"|"' + catalog[product]["desc"] + '"|"' + catalog[product]["code"] + '"')
+            # Gather coupon-item's URL
+            cUrl = c.a['href']
 
+            # Extract coupon-item's SKU
+            cSku = cUrl.split("-")[-1].split(".")[-2]
+
+            # Request coupon-page
+            cRes = requests.get(prefix + cUrl)
+            try:
+                cRes.raise_for_status()
+            except requests.packages.urllib3.exceptions.MaxRetryError as e:
+                print("Request failed. Status code: ", r.status_code())
+                sitecheck(prefix + cUrl)
+            
+            # Create coupon-page soup
+            rSoup = BeautifulSoup(cRes.text, 'lxml')
+            
+            # Harvest coupon-code
+            cCode = rSoup.select_one("#pricematching_price_coupon_code").string
+            print('CpnCode=', cCode)
+            
+            # Calculate savings
+            cPriceReg = rSoup.select_one('meta[property="og:price:amount"]')['content']
+            cPriceSale = rSoup.select_one("#pricematching_price_value").string
+            cSave = "{:.2f}".format(float(cPriceReg) - float(cPriceSale))
+            print('Savings=', cSave)
+
+            # Clean-up
+            rSoup.decompose()
+            cRes.close()
+
+            ## Aggregate SKUs
+            cSkus = []
+            aQuery = '"' + cText + '"'
+            aUrl = "/catalogsearch/result/index/?q="
+            aRes = requests.get(prefix + aUrl + aQuery)
+            try:
+                aRes.raise_for_status()
+            except requests.packages.urllib3.exceptions.MaxRetryError as e:
+                print("Request failed. Status code: ", r.status_code())
+                sitecheck(prefix + aUrl + aQuery)
+            aSoup = BeautifulSoup(aRes.text, "lxml")
+            aSkus = aSoup.find_all("div", class_="product-ids")
+            for aSku in aSkus:
+                cSkus.append(aSku.get_text().split('#')[1])
+            for itemNum in cSkus:
+                csvWriter.writerow([itemNum, cText, cCode, cSave])
+                print(itemNum, '|', cText, '|', cCode, '|', cSave)
+            print()
+            aSoup.decompose()
+            aRes.close()
+            
+        except:
+            pass
+    csvFile.close()
+s.decompose()
+res.close()
+# TODO: 
+
+# TODO: Put all data on a row .. SKU | DESC | SAVE | CODE
+
+# TODO: Sort data by sku
+
+# TODO: Save data to CSV
+
+print("--------------- Done. --------------")
